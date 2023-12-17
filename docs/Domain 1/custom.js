1ef8c7acb05c9e9a108b6366e8bbb9af26b38d62
Georgette Webb <georgette.webb78@email.com>
2023-12-17 18:07:55 +1030

    css

--- docs/Domain 1/custom.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const h1Elements = document.querySelectorAll('h1');
-    h1Elements.forEach(el => {
-        if (el.textContent === 'Index') {
-            el.style.display = 'none';
-        }
-    });
-});
